c385a34a0519ec391d37bb2c942076fc737c2e07
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-06-29 01:30:55 +0900

    feat: 프로그램 진행을 보여주기 위한 View 객체 추가

---
+++ src/main/kotlin/rss/view/KeywordSearchOutputView.kt
@@ -0,0 +1,19 @@
+package rss.view
+
+import rss.domain.post.Post
+
+object KeywordSearchOutputView {
+    fun logContentsByKeyword(keyword: String, posts: List<Post>) {
+        println("\n<$keyword> 검색 결과 : ${posts.size}건")
+        println("=================================================================================================")
+        posts.forEachIndexed { index, post ->
+            println("검색 결과 #${index + 1}")
+            println("게시글 제목 : ${post.metaData.title}")
+            println("게시글 링크 : ${post.metaData.postUrl}")
+            println("게시일 : ${post.metaData.pubDate}")
+            println("미리보기 : ${post.content.split("\n").firstOrNull()}")
+            if (index != posts.size - 1) println()
+        }
+        println("=================================================================================================")
+    }
+}
---
+++ src/main/kotlin/rss/view/LoadingView.kt
@@ -0,0 +1,7 @@
+package rss.view
+
+object LoadingView {
+    fun showLoading() {
+        println("데이터를 불러오고 있습니다...")
+    }
+}
