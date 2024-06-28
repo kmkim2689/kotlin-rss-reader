588830a670eb02d0f129e79e7785cc217238fe26
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-06-28 22:14:42 +0900

    refactor: xml document를 여러 번 파싱하는 작업을 각각의 코루틴에서 병렬적으로 수행하도록 변경

--- src/main/kotlin/rss/data/datasource/DefaultBlogRemoteDataSource.kt
+++ src/main/kotlin/rss/data/datasource/DefaultBlogRemoteDataSource.kt
@@ -1,54 +1,74 @@
 package rss.data.datasource
 
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.withContext
+import org.w3c.dom.Document
 import rss.data.model.BlogPostResponse
 import rss.data.model.BlogResponse
-import rss.data.model.Image
+import rss.data.model.ImageResponse
 import javax.xml.parsers.DocumentBuilderFactory
 
 class DefaultBlogRemoteDataSource : BlogRemoteDataSource {
     private val factory = DocumentBuilderFactory.newInstance()
 
     override suspend fun postsByUrl(url: String): BlogResponse = withContext(Dispatchers.IO) {
-
         val xml = factory.newDocumentBuilder().parse(url)
         val channels = xml.getElementsByTagName("channel")
 
         val channelNode = channels.item(0)
         val channelDocument = channelNode.ownerDocument
-        val title = channelDocument.getElementsByTagName("title").item(0).firstChild.textContent
-        val link = channelDocument.getElementsByTagName("link").item(0).firstChild.textContent
-        val description = channelDocument.getElementsByTagName("description").item(0).firstChild.textContent
-        val lastBuildDate = channelDocument.getElementsByTagName("lastBuildDate").item(0).firstChild.textContent
+        val title = channelDocument.parseTextContent("title", 0)
+        val link = channelDocument.parseTextContent("link", 0)
+        val description = channelDocument.parseTextContent("description", 0)
+        val lastBuildDate = channelDocument.parseTextContent("lastBuildDate", 0)
 
-        val image = channelDocument.getElementsByTagName("image")
-
-        val imageDocument = image.item(0).ownerDocument
-        val imageUrl = imageDocument.getElementsByTagName("url").item(0).firstChild.textContent
-        val imageTitle = imageDocument.getElementsByTagName("title").item(0).firstChild.textContent
-        val imageLink = imageDocument.getElementsByTagName("link").item(0).firstChild.textContent
-
-        val imageResponse = Image(imageTitle, imageUrl, imageLink)
+        val imageNode = channelDocument.getElementsByTagName("image").item(0)
+        val imageDocument = imageNode.ownerDocument
+        val imageUrl = imageDocument.parseTextContent("url", 0)
+        val imageTitle = imageDocument.parseTextContent("title", 0)
+        val imageLink = imageDocument.parseTextContent("link", 0)
+        val imageResponse = ImageResponse(imageTitle.await(), imageUrl.await(), imageLink.await())
 
         val itemTags = channelDocument.getElementsByTagName("item")
 
-        val items = (0..<itemTags.length).map {
-            val itemDocument = itemTags.item(it).ownerDocument
-            val itemTitle = itemDocument.getElementsByTagName("title").item(0).firstChild.textContent
-            val itemLink = itemDocument.getElementsByTagName("link").item(0).firstChild.textContent
-            val itemPubDate = itemDocument.getElementsByTagName("pubDate").item(0).firstChild.textContent
-            val itemDescription = itemDocument.getElementsByTagName("description").item(0).firstChild.textContent
-            BlogPostResponse(itemTitle, itemLink, itemPubDate, itemDescription)
-        }
+        val items = (0..<itemTags.length).map { index ->
+            async {
+                withContext(Dispatchers.Default) {
+                    val itemDocument = itemTags.item(index).ownerDocument
+                    val itemTitle = itemDocument.parseTextContent("title", 0)
+                    val itemLink = itemDocument.parseTextContent("link", 0)
+                    val itemPubDate = itemDocument.parseTextContent("pubDate", 0)
+                    val itemDescription = itemDocument.parseTextContent("description", 0)
+                    BlogPostResponse(itemTitle.await(), itemLink.await(), itemPubDate.await(), itemDescription.await())
+                }
+            }
+        }.awaitAll()
 
         BlogResponse(
-            title = title,
-            link = link,
-            description = description,
-            lastBuildDate = lastBuildDate,
-            image = imageResponse,
+            title = title.await(),
+            link = link.await(),
+            description = description.await(),
+            lastBuildDate = lastBuildDate.await(),
+            imageResponse = imageResponse,
             items = items
         )
+    }
+
+    private suspend fun Document.parseTextContent(
+        tagName: String,
+        index: Int = 0,
+    ): Deferred<String> {
+        return withContext(Dispatchers.Default) {
+            async {
+                this@parseTextContent
+                    .getElementsByTagName(tagName)
+                    .item(index)
+                    .firstChild
+                    .textContent
+            }
+        }
     }
 }
